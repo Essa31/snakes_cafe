b7555578343a84384f049e19197f4de721eb1117
Essa31 <esaaeyad22@gmail.com>
2022-05-30 00:03:59 -0700

    snakes_cafe

---
+++ snakes_cafe.py
@@ -0,0 +1,51 @@
+print(""" $ python snakes_cafe.py
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+"""
+      )
+order = input("""
+***********************************
+** What would you like to order? **
+***********************************
+> """
+    )
+while order!="quit":
+    n =int(input("number of order ? "))
+    print(f'** {n} order of {order} have been added to your meal **')
+    order = input("""
+***********************************
+** What would you like to order? **
+***********************************
+> """)
+else:
+    print("Exit")
